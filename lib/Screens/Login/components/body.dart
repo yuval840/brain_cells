ce20be833f450727488f921f485ea2b522f470c6
yuval840 <yuval840@gmail.com>
2022-12-21 16:47:05 +0200

    data base trying

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,4 +1,9 @@
 import 'package:brain_cells/Screens/Login/components/background.dart';
+<<<<<<< Updated upstream
+=======
+import 'package:brain_cells/Screens/Profile/profile_page_screen.dart';
+import 'package:brain_cells/Screens/Signup/signup_screen.dart';
+>>>>>>> Stashed changes
 import 'package:brain_cells/components/already_have_an_account_check.dart';
 import 'package:brain_cells/components/rounded_input_field.dart';
 import 'package:brain_cells/components/text_field_container.dart';
@@ -6,7 +11,12 @@ import 'package:brain_cells/constants.dart';
 import 'package:brain_cells/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+<<<<<<< Updated upstream
 
+=======
+import 'package:brain_cells/Screens/Signup/signup_screen.dart';
+import 'package:brain_cells/Screens/Profile/profile_page_screen.dart';
+>>>>>>> Stashed changes
 import '../../../components/rounded_password_field.dart';
 
 class Body extends StatelessWidget {
@@ -18,6 +28,7 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
+<<<<<<< Updated upstream
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -41,6 +52,64 @@ class Body extends StatelessWidget {
             press: () {},
           ),
         ],
+=======
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            SvgPicture.asset(
+              "assets/icons/braincaell-logo2.svg",
+              height: size.height * 0.4,
+            ),
+            SizedBox(
+              height: size.height * 0.001,
+            ),
+            RoundedInputField(
+              hintText: "Email address",
+              onChanged: (value) {},
+              key: null,
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ProfilePageScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+>>>>>>> Stashed changes
       ),
     );
   }
---
+++ lib/Screens/Profile/profile_page_screen.dart
@@ -0,0 +1,13 @@
+import 'package:brain_cells/Screens/Profile/components/body.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePageScreen extends StatelessWidget {
+  const ProfilePageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
